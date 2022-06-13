32812e63867fa649af1f794746bdd371c8f14ee2
kaquoc <kaquoc1206@gmail.com>
2022-06-13 16:14:02 -0400

    create constructure Aircraft

--- app/src/main/java/com/example/flightpointerbeta/Aircraft.java
+++ app/src/main/java/com/example/flightpointerbeta/Aircraft.java
@@ -1,4 +1,34 @@
 package com.example.flightpointerbeta;
 
-public class Aircraft {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+class Aircraft {
+    String ac_callsign;
+    String ac_icao;
+    String ac_reg;
+    String ac_type;
+    double ac_lat;
+    double ac_long;
+    double dist;
+    double alt; //altitude in ft
+
+    public Aircraft(JSONObject aircraft,double di) {
+        try{
+            ac_callsign = aircraft.getString("call");
+            ac_icao = aircraft.getString("icao");
+            ac_reg = aircraft.getString("reg");
+            ac_type = aircraft.getString("type");
+            ac_lat  = Double.parseDouble(aircraft.getString("lat"));
+            ac_long = Double.parseDouble(aircraft.getString("lon"));
+            dist = di;
+
+        }catch (JSONException e){
+
+        }
+    }
+    /**calculating distance to user using pythagoras theorem and haversine formula*/
+    public double distance_to_user(){
+
+    }
 }
